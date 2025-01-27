4e9bcfa77d2868ea2346071c4cb81d727e6535f3
belleyy <geanniebelletero@gmail.com>
2025-01-27 18:47:41 +0800

    Performance Task Python Project.py

--- Performance Task Python Project.py
+++ Performance Task Python Project.py
@@ -1,19 +1,20 @@
+# Setup string variables
 operator = input("Enter an operator (+ - * /): ")
 number1 = float(input("Enter the 1st number: "))
 number2 = float(input("Enter the 2nd number: "))
 
-if operator == "+":
+if operator == "+":    # This operator adds numbers
     result = number1 + number2
     print(round(result, 3))
-elif operator == "-":
+elif operator == "-":  # This operator subtracts numbers
     result = number1 - number2
     print(round(result, 3))
-elif operator == "*":
+elif operator == "*":  # This operator multiplies numbers
     result = number1 * number2
     print(round(result, 3))
-elif operator == "/":
-    result = number1 / number2
+elif operator == "/":  # This operator divides numbers
+    result = number1 / number2 
     print(round(result, 3))
-else:
+else:                  # It results to this syntaxerror when certain invalid numbers or operators are given by users
     print(f"{operator} is not a valid operator")
     
